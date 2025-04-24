e785767da7d9e1139911a3d9b4ea0dc30f4cc85a
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-24 15:59:13 -0300

    primeiro commit

---
+++ operacoes.go
@@ -0,0 +1,17 @@
+package main
+
+func soma(x, y int) int{
+	return x + y
+}
+
+func subitrair(x, y int) int{
+	return x - y
+}
+
+func multiplicao(x, y int) int{
+	return x * y
+}
+
+func divisao(x, y int) int{
+	return x / y
+}
